00c45a7f846be0f07775203dae46c6b87d0b0edb
Rasoul <rasolmarzban@outlook.com>
2024-04-30 13:06:35 +0330

    initial

---
+++ core.village.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package CarVillage      
+ */
+/*
+Plugin Name: Addon Plugin For Theme
+Plugin URI: https://github.com/rasolmarzban/carvillage.git
+Description: This Plug for every change the customer wanna do in the website
+Version: 0.0.1
+Requires at least: 0.0.1
+Requires PHP: 5.6.20
+Author: Rasoul Marzban
+Author URI: https://github.com/rasolmarzban
+License: nothing
+Text Domain: carvillage
+*/
+// Make sure we don't expose any info if called directly
